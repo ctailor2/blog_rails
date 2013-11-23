5959b031e693adcb9f3cd2ad3d528db4ab5625e2
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-11-23 12:19:46 -0600

    Added tagging and tags models/migrations, and wired them up

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
   has_many :comments, dependent: :destroy
+  has_many :taggings
+  has_many :tags, :through => :taggings
 
   validates :title, presence: true, length: { minimum: 5 }
 end
